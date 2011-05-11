fefe071b6a8997be2620641bec064b095e093fa9
Evan Walsh <evan@nothingconcept.com>
2011-05-10 22:17:31 -0400

    HEROKU STUFF

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,16 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'uri'
+
+if ENV["MONGOHQ_URL"]
+  mongo_uri = URI.parse(ENV["MONGOHQ_URL"])
+  ENV['MONGOID_HOST'] = mongo_uri.host
+  ENV['MONGOID_PORT'] = mongo_uri.port.to_s
+  ENV["MONGOID_USERNAME"] = mongo_uri.user
+  ENV['MONGOID_PASSWORD'] = mongo_uri.password
+  ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub("/", "")
+end
+
 # Pick the frameworks you want:
 # require "active_record/railtie"
 require "action_controller/railtie"
