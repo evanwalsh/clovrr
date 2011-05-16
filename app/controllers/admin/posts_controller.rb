3a398717482125fd7e62743966a4d71900ff6baa
Evan Walsh <evan@nothingconcept.com>
2011-05-16 01:34:03 -0400

    Preferences.

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -4,9 +4,32 @@ class Admin::PostsController < AdminController
   end
 
   def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new params[:post]
+    @post.user = current_user
+    if @post.save
+      redirect_to admin_posts_url, notice: 'Post published.'
+    else
+      flash[:error] = 'Post could not be published.'
+      render :new
+    end
   end
 
   def edit
+    @post = Post.where(url: params[:id]).first
+  end
+  
+  def update
+    @post = Post.where(url: params[:id]).first
+    if @post.update_attributes params[:post]
+      redirect_to admin_posts_url, notice: 'Post saved.'
+    else
+      flash[:error] = 'Post could not be saved.'
+      render :edit
+    end
   end
 
 end
--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,11 +1,34 @@
 class Admin::PreferencesController < AdminController
   def index
+    @preferences = Preference.all
   end
 
   def new
+    @preference = Preference.new
+  end
+  
+  def create
+    @preference = Preference.new params[:preference]
+    @preference.user = current_user
+    if @preference.save
+      redirect_to admin_preferences_url, notice: 'Preference published.'
+    else
+      flash[:error] = 'Preference could not be created.'
+      render :new
+    end
   end
 
   def edit
+    @preference = Preference.where(url: params[:id]).first
   end
-
+  
+  def update
+    @preference = Preference.where(url: params[:id]).first
+    if @preference.update_attributes params[:preference]
+      redirect_to admin_preferences_url, notice: 'Preference saved.'
+    else
+      flash[:error] = 'Preference could not be saved.'
+      render :edit
+    end
+  end
 end
